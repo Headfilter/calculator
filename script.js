400c9ae90058fc2cacd45033c16094ddb490c0e6
Nick <allthingsmisc01@gmail.com>
2026-04-06 09:56:54 -0700

    Mostly finished project

--- script.js
+++ script.js
@@ -1,25 +1,52 @@
-let num1 = 0;
+let num1 = '';
+let operator = null;
+let num2 = '';
 
-let operator = 0;
+const display = document.querySelector(".calcScreen");
+const buttons = document.querySelectorAll(".button")
 
-let num2 = 0;
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        const value = button.innerText;
+        const op = button.dataset.operator;
+        const action = button.dataset.action;
+        if (!action && !op) {
+            if (num1 === '0') num1 = value;
+            else num1 += value;
+            updateDisplay(num1);
+        }
+        if (op) {
+            operator = op;
+            num2 = num1;
+            num1 = '';
+        }
+        if (button.id === 'equals') {
+            const result = calculate(parseFloat(num2), parseFloat(num1), operator);
+            updateDisplay(result);
+            num1 = result;
+        }
+        if (action === "clear") {
+            num1 = '0';
+            num2 = '';
+            operator = null;
+            updateDisplay(num1);
+        }
+        if (action === "delete") {
+            if(num1.length > 0) {
+                num1 = num1.slice(0, -1);
+            }
+            updateDisplay(num1);
+        }
+    });
+});
 
-const add = function (x, y) {
-    return x + y;
-};
+function updateDisplay(val) {
+    display.innerText = val;
+}
 
-const subrtact = function (x, y) {
-    return x - y;
-};
-
-const multiply = function (x, y) {
-    return x * y;
-};
-
-const divide = function (x, y) {
-    return x / y;
-};
-
-function operate(func, x, y) {
-    return func(x, y);
+function calculate(x, y, op) {
+    if(op === '+') return x + y;
+    if(op === '-') return x - y;
+    if(op === '*') return x * y;
+    if(op === '/') return x / y;
 };
